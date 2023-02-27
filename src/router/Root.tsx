f1dd5255fa01209b864bd30931020ed9776cacc3
crystal <oj3339@naver.com>
2023-02-27 20:12:14 +0900

    #4 ADD Root.tsx 사이드바 숨기기 기능 추가

--- src/router/Root.tsx
+++ src/router/Root.tsx
@@ -1,10 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import { SideNav } from 'components/bar/sideBar/SideNav';
+import { useEffect, useState } from 'react';
 
 export const Root = () => {
+  const [state, setState] = useState(false);
+  const path = window.location.pathname;
+
+  useEffect(() => {
+    if (window.location.pathname !== '/') {
+      setState(true);
+    } else {
+      setState(false);
+    }
+  }, [path]);
   return (
     <div>
-      <SideNav />
+      {state && <SideNav />}
       <Outlet />
     </div>
   );
