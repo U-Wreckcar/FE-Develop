6fe0cab96af56bce6187a9f67c22b399d7ca0479
crystal <oj3339@naver.com>
2023-03-03 12:17:05 +0900

    #24 ADD login style 추가

--- src/components/filter_box/FilterBox.tsx
+++ src/components/filter_box/FilterBox.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { CreateAt } from './CreateAt';
 import { Source } from './Source';
 import { Medium } from './Medium';
 import { KeyWord } from './KeyWord';
 import styles from './filterBox.module.css';
+import axios from 'axios';
+
 export default function FilterBox() {
   const cadsf = [<CreateAt />, <Source />, <Medium />, <KeyWord />];
 
+  const getCode = async () => {
+    if (window.location.search !== '') {
+      const code = window.location.search.split('?code=')[1];
+      await axios.get(
+        `https://dee8-14-6-160-238.jp.ngrok.io/auth/kakao/callback?code=${code}`
+      );
+    }
+  };
+
+  useEffect(() => {
+    getCode();
+  }, []);
   return (
     <div className={styles.container}>
       {cadsf.map((d) => {
