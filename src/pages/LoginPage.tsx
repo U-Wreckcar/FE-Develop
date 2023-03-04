8703c262effafc89737430738bc241d3b0c42ee4
crystal <oj3339@naver.com>
2023-03-04 20:04:54 +0900

    REMOVE mui 삭제

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,12 @@
 import LoginBox from 'components/login_component/LoginBox';
+import TestTable from 'components/login_component/TestTable';
 
 const LoginPage = () => {
   return (
     <div>
       login Page
       <LoginBox />
+      <TestTable />
     </div>
   );
 };
