589ece4ab6ed1c3777f0b2d84552d9af21dcd31e
crystal <oj3339@naver.com>
2023-03-10 10:04:19 +0900

    #93 EDIT render page header

--- src/router/Root.tsx
+++ src/router/Root.tsx
@@ -2,6 +2,8 @@ import { Outlet } from 'react-router-dom';
 import { SideNav } from 'components/bar/sideBar/SideNav';
 import { useEffect, useState } from 'react';
 import { Header } from 'components/bar/header/Header';
+import { RenderHeader } from 'components/bar/header/RenderHeader';
+import { LoginHeader } from 'components/bar/header/LoginHeader';
 
 export const Root = () => {
   const [state, setState] = useState(false);
@@ -16,8 +18,14 @@ export const Root = () => {
   }, [path]);
   return (
     <div>
-      {state && <SideNav />}
-      <Header />
+      {state && (
+        <>
+          <SideNav />
+          <Header />
+        </>
+      )}
+      {/* <RenderHeader /> */}
+      <LoginHeader />
       <Outlet />
     </div>
   );
